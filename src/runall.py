61bcddebc349018c5345773149cc8aad89f72b62
unknown <seifibrahim10@gmail.com>
2015-08-21 15:22:15 -0700

    finally caught up

--- src/runall.py
+++ src/runall.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Script to run entire SN analysis."""
 
 import os
@@ -17,3 +16,21 @@ results_fname = os.path.join("..", "results", "results.txt")
 if not os.path.exists(data_fname):
     print("downloading data...")
     data_utils.download_file(DATA_URL, data_fname)
+
+sne = data_utils.read_sn_data(data_fname)
+
+# Correct mb with known correction factors.
+sne["mb_corr"] = sne["mb"] - 2.3 * sne["c"] + 0.128 * sne["x1"]
+
+print("Fitting cosmology to data...")
+cosmo = fitting.fit_cosmology(sne["z"], sne["mb_corr"], sne["mb_err"])
+
+print("Plotting results to", plot_fname)
+fig = fitting.plot_sn_data(sne["z"], sne["mb_corr"], sne["mb_err"],
+                           cosmo=cosmo)
+fig.savefig(plot_fname)
+
+print("Writing results file to", results_fname)
+with open(results_fname, "w") as f:
+    f.write("H0,Om0\n{},{}\n".format(cosmo.H0.value, cosmo.Om0))
+
